fa38ebff60a90d0c8c6a1dae4c725787db1750b3
Cristian Diaz <diazcristian0210@gmail.com>
2024-07-25 20:18:01 -0300

    Agregue algunas validaciones como minimo de caracteres, maximo de caracteres y un includes que hace que no se puedan repetir tareas ya agregadas, lo visualicé con un alerta. Utilicé react hook form

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -1,49 +1,84 @@
 import { Button, Form } from "react-bootstrap";
 import Listatareas from "./Listatareas";
-import { useEffect ,useState } from "react";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
 const FormularioTareas = () => {
-  const [nombreTarea, setNombreTarea ] = useState('')
-  const [tareas, setTareas] = useState( JSON.parse(localStorage.getItem('listaTareasKey')) || [] );
+  const [tareas, setTareas] = useState(
+    JSON.parse(localStorage.getItem("listaTareasKey")) || []
+  );
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
 
+  // aqui agrego el codigo del ciclo de vida
+  useEffect(() => {
+    //aqui agrear el codigo que quiero ejecutar en el montaje y act. del componente
+    console.log("estoy dentro del useEffect");
+    localStorage.setItem("listaTareasKey", JSON.stringify(tareas));
+  }, [tareas]);
 
+  const onSubmit = (data) => {
+    console.log(data);
+    //verificar si la tarea ya existe en array de tareas
+    if(tareas.includes(data.nombreTarea)){
+      alert("La tarea ya existe")
+    }else{
+      setTareas([...tareas, data.nombreTarea]); //operador spread
+      reset();
+    }
 
-  //Aqui agrego el ciclo de vida de un componente useEffect
-  useEffect(()=>{
-    //aqui agregar el codigo que quiero ejecutar en el montaje y actualizacion del componente
-    console.log('Estoy dentro del useEffect')
-    localStorage.setItem('listaTareasKey', JSON.stringify(tareas))
-  }, [tareas])
-
-  const handleSubmit = (e)=> {
-    e.preventDefault();
-    setTareas([...tareas, nombreTarea]); // Operador spread
+    // tareas.push(nombreTarea)
+    // 
     //limpiar el input
-    setNombreTarea('');
-  }
+    // setNombreTarea("");
+  };
 
-  const borrarTarea = (nombre) =>{
-    const tareasActualizadas = tareas.filter((itemTarea) => itemTarea !== nombre) 
-    //Actualizar el state tareas. Siempre usando el SET
+  const borrarTarea = (nombre) => {
+    const tareasActualizadas = tareas.filter(
+      (itemTarea) => itemTarea !== nombre
+    );
+    // actualizar el state tareas
     setTareas(tareasActualizadas);
-  }
+  };
 
   return (
     <section>
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <Form.Group className="mb-3 d-flex" controlId="tarea">
           <Form.Control
             type="text"
-            placeholder="Ej: Añade tu nueva tarea"
-            minLength={3}
-            maxLength={50}
-          onChange= {(e)=> setNombreTarea(e.target.value)}
-          value = {nombreTarea}
+            placeholder="Ej: tarea 1"
+            // minLength={3}
+            // maxLength={50}
+            // onChange={(e) => setNombreTarea(e.target.value)}
+            // value={nombreTarea}
+            {...register("nombreTarea", {
+              required: "El nombre de la tarea es obligatorio.",
+              minLength: {
+                value: 3,
+                message:
+                  "El nombre de la tarea debe contener 3 caracteres como minimo.",
+              },
+              maxLength: {
+                value: 50,
+                message:
+                  "El nombre de la tarea debe contener hasta 50 caracteres como maximo.",
+              },
+            })}
           />
-          <Button variant="primary" className="mx-2" type="submit">Enviar</Button>
+          <Button variant="primary" className="mx-2" type="submit">
+            Enviar
+          </Button>
         </Form.Group>
+        <Form.Text className="text-light">
+          {errors.nombreTarea?.message}
+        </Form.Text>
       </Form>
-      <Listatareas tareasProps={tareas} borrarActividadProps={borrarTarea} ></Listatareas>
+      <Listatareas tareasProps={tareas} borrarActividadProps={borrarTarea}></Listatareas>
     </section>
   );
 };
